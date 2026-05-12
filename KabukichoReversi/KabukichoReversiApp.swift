5c36ead58276cd07144efadbc3c47d677f1e899c
snarfnet <82538502+snarfnet@users.noreply.github.com>
2026-05-12 17:20:20 +0900

    fix: move MobileAds init to App.init, use onAppear for ATT

--- KabukichoReversi/KabukichoReversiApp.swift
+++ KabukichoReversi/KabukichoReversiApp.swift
@@ -4,22 +4,16 @@ import GoogleMobileAds
 
 @main
 struct KabukichoReversiApp: App {
-    @Environment(\.scenePhase) private var scenePhase
-    @State private var attRequested = false
+    init() {
+        MobileAds.shared.start(completionHandler: nil)
+    }
 
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .onChange(of: scenePhase) {
-                    if scenePhase == .active && !attRequested {
-                        attRequested = true
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-                            ATTrackingManager.requestTrackingAuthorization { _ in
-                                DispatchQueue.main.async {
-                                    MobileAds.shared.start { _ in }
-                                }
-                            }
-                        }
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                        ATTrackingManager.requestTrackingAuthorization { _ in }
                     }
                 }
         }
